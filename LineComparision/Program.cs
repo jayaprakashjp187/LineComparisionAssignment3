9bb49208bef1149deeb2e756b98fb34337ce3229
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-11 11:09:52 +0530

    checking two length of lines equal or not

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -11,7 +11,12 @@ namespace LineComparision
             float x2;
             float y1;
             float y2;
-            float length;
+            float x3;
+            float x4;
+            float y3;
+            float y4;
+            float length1;
+            float length2;
             Console.WriteLine("enter the x1 value");
             float.TryParse(Console.ReadLine(), out x1);
             Console.WriteLine("enter the x2 value");
@@ -20,10 +25,30 @@ namespace LineComparision
             float.TryParse(Console.ReadLine(), out y1);
             Console.WriteLine("enter the y2 value");
             float.TryParse(Console.ReadLine(), out y2);
-            length = (float) Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("length of co-ordinates is");
-            Console.WriteLine(length);
-              
+            Console.WriteLine("enter the x3 value");
+            float.TryParse(Console.ReadLine(), out x3);
+            Console.WriteLine("enter the x4 value");
+            float.TryParse(Console.ReadLine(), out x4);
+            Console.WriteLine("enter the y3 value");
+            float.TryParse(Console.ReadLine(), out y3);
+            Console.WriteLine("enter the y4 value");
+            float.TryParse(Console.ReadLine(),out y4);
+
+            length1 = (float) Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1, 2));
+            length2 = (float)Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("length of first co-ordinates  is");
+            Console.WriteLine(length1);
+            Console.WriteLine("length of second co-ordinates  is");
+            Console.WriteLine(length2);
+            if(length1 ==length2)
+            {
+                Console.WriteLine("two length of lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("two lenth of lines are not equal");
+            }
+
         }
     }
 }
